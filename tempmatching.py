44045a4e3d8f84584540c5c5f1001f1bd8430587
jaw kaixuan <j_kaixuan@live.com>
2019-03-23 22:31:45 +0800

    template matching updated input

--- tempmatching.py
+++ tempmatching.py
@@ -20,19 +20,48 @@ matched = 0
 
 array_draw_color = [ (255,0,0) , (0,255,0) , (0,0,255) , (0,255,255)]
 
-tgoal0 = cv2.imread('template/_goalpost/0.jpg',0)
-tgoal1 = cv2.imread('template/_goalpost/1.jpg',0)
-tgoal2 = cv2.imread('template/_goalpost/2.jpg',0)
+tangle0 = cv2.imread('template/t_angle/0.jpg',0)
+tangle1 = cv2.imread('template/t_angle/1.jpg',0)
+tangle2 = cv2.imread('template/t_angle/2.jpg',0)
 
-trdd0 =  cv2.imread('template/_rdd/0.jpg',0)
-trdd1 =  cv2.imread('template/_rdd/1.jpg',0)
-trdd2 =  cv2.imread('template/_rdd/2.jpg',0)
+tcolorblue0 = cv2.imread('template/t_colorblue/0.jpg',0)
+tcolorblue1 = cv2.imread('template/t_colorblue/1.jpg',0)
+tcolorblue2 = cv2.imread('template/t_colorblue/2.jpg',0)
 
-tcshape0 = cv2.imread('template/_cshape/0.jpg',0)
-tcshape1 = cv2.imread('template/_cshape/1.jpg',0)
-tcshape2 = cv2.imread('template/_cshape/2.jpg',0)
+tcolorgreen0 = cv2.imread('template/t_colorgreen/0.jpg',0)
+tcolorgreen1 = cv2.imread('template/t_colorgreen/1.jpg',0)
+tcolorgreen2 = cv2.imread('template/t_colorgreen/2.jpg',0)
 
-goal_array_name = [ tgoal0 , tgoal1 , tgoal2 , 'goal']
+tcolorred0 = cv2.imread('template/t_colorred/0.jpg',0)
+tcolorred1 = cv2.imread('template/t_colorred/1.jpg',0)
+tcolorred2 = cv2.imread('template/t_colorred/2.jpg',0)
+
+tcoloryellow0 = cv2.imread('template/t_coloryellow/0.jpg',0)
+tcoloryellow1 = cv2.imread('template/t_coloryellow/1.jpg',0)
+tcoloryellow2 = cv2.imread('template/t_coloryellow/2.jpg',0)
+
+tcshape0 = cv2.imread('template/t_cshape/0.jpg',0)
+tcshape1 = cv2.imread('template/t_cshape/1.jpg',0)
+tcshape2 = cv2.imread('template/t_cshape/2.jpg',0)
+
+tgoalpost0 = cv2.imread('template/t_goalpost/0.jpg',0)
+tgoalpost1 = cv2.imread('template/t_goalpost/1.jpg',0)
+tgoalpost2 = cv2.imread('template/t_goalpost/2.jpg',0)
+
+tcshape0 = cv2.imread('template/t_cshape/0.jpg',0)
+tcshape1 = cv2.imread('template/t_cshape/1.jpg',0)
+tcshape2 = cv2.imread('template/t_cshape/2.jpg',0)
+
+trdd0 =  cv2.imread('template/t_rdd/0.jpg',0)
+trdd1 =  cv2.imread('template/t_rdd/1.jpg',0)
+trdd2 =  cv2.imread('template/t_rdd/2.jpg',0)
+
+ttfl0 =  cv2.imread('template/t_tfl/0.jpg',0)
+ttfl1 =  cv2.imread('template/t_tfl/1.jpg',0)
+ttfl2 =  cv2.imread('template/t_tfl/2.jpg',0)
+
+
+goal_array_name = [ tgoalpost0 , tgoalpost1 , tgoalpost2 , 'goal']
 rdd_array_name = [ trdd0 , trdd1 , trdd2 , 'read distance']
 cshape_array_name = [ tcshape0 , tcshape1 , tcshape2 , 'countshape']
 
