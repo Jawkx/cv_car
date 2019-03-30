a66cf22d8f689ee5d6ee9d126c0ba6282005ae2d
Jaw <j_kaixuan@live.com>
2019-03-30 22:47:33 +0800

    added readangle and readdistance into final code

--- finalcode.py
+++ finalcode.py
@@ -13,8 +13,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 servoPin = 17
 trigger = 21 
-echo = 20  
-
+echo = 20
 
 GPIO.setup(servoPin, GPIO.OUT)
 GPIO.setup(trigger,GPIO.OUT)
@@ -25,6 +24,9 @@ p.start(2.3) # Initialization
 ##SETTING FOR I2C
 bus = smbus.SMBus(1)
 car_address = 0x04
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+address = 0x68  
 
 ##GENERAL VARIABLE
 action = 0
@@ -32,25 +34,59 @@ action = 0
 ##VARIABLE FOR TEMPLATE RECOGNITION
 i = 0
 j = 0
+
+matching_name = "none"
+
 array_draw_color = [ (255,0,0) , (0,255,0) , (0,0,255) , (0,255,255)]
 
-tgoal0 = cv2.imread('t_goalpost/0.jpg',0)
-tgoal1 = cv2.imread('t_goalpost/1.jpg',0)
-tgoal2 = cv2.imread('t_goalpost/2.jpg',0)
+tangle0 = cv2.imread('template/t_angle/0.jpg',0)
+tangle1 = cv2.imread('template/t_angle/1.jpg',0)
+tangle2 = cv2.imread('template/t_angle/2.jpg',0)
 
-trdd0 =  cv2.imread('t_rdd/0.jpg',0)
-trdd1 =  cv2.imread('t_rdd/1.jpg',0)
-trdd2 =  cv2.imread('t_rdd/2.jpg',0)
+tcolorblue0 = cv2.imread('template/t_colorblue/0.jpg',0)
+tcolorblue1 = cv2.imread('template/t_colorblue/1.jpg',0)
+tcolorblue2 = cv2.imread('template/t_colorblue/2.jpg',0)
 
-tcshape0 = cv2.imread('t_cshape/0.jpg',0)
-tcshape1 = cv2.imread('t_cshape/1.jpg',0)
-tcshape2 = cv2.imread('t_cshape/2.jpg',0)
+tcolorgreen0 = cv2.imread('template/t_colorgreen/0.jpg',0)
+tcolorgreen1 = cv2.imread('template/t_colorgreen/1.jpg',0)
+tcolorgreen2 = cv2.imread('template/t_colorgreen/2.jpg',0)
 
-goal_array_name = [ tgoal0 , tgoal1 , tgoal2 , 'goal']
-rdd_array_name = [ trdd0 , trdd1 , trdd2 , 'read distance']
-cshape_array_name = [ tcshape0 , tcshape1 , tcshape2 , 'countshape']
+tcolorred0 = cv2.imread('template/t_colorred/0.jpg',0)
+tcolorred1 = cv2.imread('template/t_colorred/1.jpg',0)
+tcolorred2 = cv2.imread('template/t_colorred/2.jpg',0)
 
-match_for_name = [goal_array_name , rdd_array_name , cshape_array_name ] 
+tcoloryellow0 = cv2.imread('template/t_coloryellow/0.jpg',0)
+tcoloryellow1 = cv2.imread('template/t_coloryellow/1.jpg',0)
+tcoloryellow2 = cv2.imread('template/t_coloryellow/2.jpg',0)
+
+tcshape0 = cv2.imread('template/t_cshape/0.jpg',0)
+tcshape1 = cv2.imread('template/t_cshape/1.jpg',0)
+tcshape2 = cv2.imread('template/t_cshape/2.jpg',0)
+
+tgoalpost0 = cv2.imread('template/t_goalpost/0.jpg',0)
+tgoalpost1 = cv2.imread('template/t_goalpost/1.jpg',0)
+tgoalpost2 = cv2.imread('template/t_goalpost/2.jpg',0)
+
+trdd0 =  cv2.imread('template/t_rdd/0.jpg',0)
+trdd1 =  cv2.imread('template/t_rdd/1.jpg',0)
+trdd2 =  cv2.imread('template/t_rdd/2.jpg',0)
+
+ttfl0 =  cv2.imread('template/t_tfl/0.jpg',0)
+ttfl1 =  cv2.imread('template/t_tfl/1.jpg',0)
+ttfl2 =  cv2.imread('template/t_tfl/2.jpg',0)
+
+angle_name = [ tangle0 , tangle1 , tangle2 , 'readangle']
+colorblue_name = [ tcolorblue0 , tcolorblue1 , tcolorblue2 , 'follow blue']
+colorgreen_name = [ tcolorgreen0 , tcolorgreen1 , tcolorgreen2 , 'follow green']
+colorred_name = [tcolorred0 , tcolorred1 , tcolorred2 , 'followred']
+coloryellow_name = [ tcoloryellow0 , tcoloryellow1 , tcoloryellow2 , 'follow yellow']
+cshape_name = [ tcshape0 , tcshape1 , tcshape2 , 'countshape']
+goal_name = [ tgoalpost0 , tgoalpost1 , tgoalpost2 , 'goal']
+rdd_name = [ trdd0 , trdd1 , trdd2 , 'read distance']
+tlf_name = [ ttfl0 , ttfl1 , ttfl2 , 'traffic light']
+
+match_for_name = [ angle_name , colorblue_name , colorgreen_name , colorred_name , coloryellow_name , cshape_name , goal_name , rdd_name ,tlf_name]
+thresholdValue = [ 0.8 , 0.8 , 0.8 , 0.7 , 0.8 , 0.9 , 0.8 , 0.9 , 0.5]
 
 
 ##VAR FOR COLOR
@@ -75,22 +111,6 @@ lastcolorblack = 1
 freeblack = 0
 freeblackcount = 0
 noblack = 0
-
-
-matchingtemplate = 0
-matchgoal = 0
-i = 0
-
-##INITIALIZE CAMERA
-camera = picamera.PiCamera()
-camera.resolution = (560,256)
-camera.framerate = 60
-rawCapture = picamera.array.PiRGBArray(camera)
-sendstatus = input('Sendstatus 1-true 0-false:')
-followblack = input('follow black 1-true 0 false :')
-stop_distance = input('stop distance (cm):')
-
-time.sleep(0.1)
 
 def calculatedistance():
 
@@ -168,6 +188,89 @@ def countSendShift(contours):
 	else:
 		return 4
 
+def readtemplate(target):
+	matched = 0
+	img = cv2.imread(target)
+	img_gray = cv2.cvtColor( img , cv2.COLOR_BGR2GRAY)
+
+	for i in range ( 0 , 8 ) :
+
+		print("i=" , i)	
+		for j in range ( 0, 3 ):
+			print("j=",j)
+			current_template = match_for_name[i][j]
+			res = cv2.matchTemplate(img_gray, current_template,cv2.TM_CCOEFF_NORMED)
+			loc = np.where( res >= thresholdValue[i])
+
+			if len( zip(*loc[::-1]) ) >= 3 :
+				matched = 1
+		
+			#for pt in zip(*loc[::-1]):
+			#   cv2.circle(img, pt, 5 ,array_draw_color[i] , -1 )
+
+		
+		if ( matched != 0 ):
+			return match_for_name[i][3]
+
+
+	return "no match"
+
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+def get_angle():
+    acceleration_xout = read_word_2c(0x3b)
+    acceleration_yout = read_word_2c(0x3d)
+    acceleration_zout = read_word_2c(0x3f)
+ 
+    acceleration_xout_scaled = acceleration_xout / 16384.0
+    acceleration_yout_scaled = acceleration_yout / 16384.0
+    acceleration_zout_scaled = acceleration_zout / 16384.0
+
+    return round(get_x_rotation(acceleration_xout_scaled, acceleration_yout_scaled, acceleration_zout_scaled))
+'''
+----------------------------------------------------------------
+================================================================
+█▀▀ ▀▀█▀▀ █▀▀█ █▀▀█ ▀▀█▀▀   █▀▀█ █▀▀█ █▀▀█ █▀▀▀ █▀▀█ █▀▀█ █▀▄▀█
+▀▀█   █   █▄▄█ █▄▄▀   █     █  █ █▄▄▀ █  █ █ ▀█ █▄▄▀ █▄▄█ █ ▀ █
+▀▀▀   ▀   ▀  ▀ ▀ ▀▀   ▀     █▀▀▀ ▀ ▀▀ ▀▀▀▀ ▀▀▀▀ ▀ ▀▀ ▀  ▀ ▀   ▀
+================================================================
+----------------------------------------------------------------
+'''
+##INITIALIZE CAMERA
+camera = picamera.PiCamera()
+camera.resolution = (560,256)
+camera.framerate = 60
+rawCapture = picamera.array.PiRGBArray(camera)
+sendstatus = input('Sendstatus 1-true 0-false:')
+followblack = input('follow black 1-true 0 false :')
+stop_distance = input('stop distance (cm):')
+
+time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture,format='bgr',use_video_port=True):
 
